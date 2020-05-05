73e40a83d9426017a22a68f029358a120fcae14d
Ankitgif <ankitsinha2303@gmail.com>
2020-05-05 21:25:14 +0530

    [Ankit] Added Wrong_Delimeter_Header_In_File custom exception

--- src/main/java/com/indiacensusanalyser/CensusAnalyserException.java
+++ src/main/java/com/indiacensusanalyser/CensusAnalyserException.java
@@ -4,7 +4,7 @@ public class CensusAnalyserException extends Exception {
     private final exceptionType type;
 
     public enum exceptionType{
-        CENSUS_FILE_PROBLEM
+        CENSUS_FILE_PROBLEM,WRONG_DELIMETER_HEADER_IN_FILE
     };
     CensusAnalyserException(exceptionType type,String message){
         super(message);
