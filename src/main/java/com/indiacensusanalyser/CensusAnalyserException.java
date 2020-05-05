fc843ef5c032682af6b4190842065fb895dabe2e
Ankitgif <ankitsinha2303@gmail.com>
2020-05-05 23:58:26 +0530

    [Ankit] Added Unable To Parse Custom Exception

--- src/main/java/com/indiacensusanalyser/CensusAnalyserException.java
+++ src/main/java/com/indiacensusanalyser/CensusAnalyserException.java
@@ -4,7 +4,7 @@ public class CensusAnalyserException extends Exception {
     private final exceptionType type;
 
     public enum exceptionType{
-        CENSUS_FILE_PROBLEM,WRONG_DELIMETER_HEADER_IN_FILE
+        CENSUS_FILE_PROBLEM,WRONG_DELIMETER_HEADER_IN_FILE,UNABLE_TO_PARSE
     };
     CensusAnalyserException(exceptionType type,String message){
         super(message);
