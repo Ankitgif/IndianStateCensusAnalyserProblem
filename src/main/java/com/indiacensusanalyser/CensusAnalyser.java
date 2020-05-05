2258aad047e6a6c0dca5d5e9f3ab1954d0cbf519
Ankitgif <ankitsinha2303@gmail.com>
2020-05-05 21:22:52 +0530

    [Ankit] Added Runtime Exception in catch clause

--- src/main/java/com/indiacensusanalyser/CensusAnalyser.java
+++ src/main/java/com/indiacensusanalyser/CensusAnalyser.java
@@ -28,6 +28,8 @@ public class CensusAnalyser {
             }
         } catch (IOException exception) {
             throw new CensusAnalyserException(CensusAnalyserException.exceptionType.CENSUS_FILE_PROBLEM,"File Not Found");
+        }  catch (RuntimeException exception){
+            throw new CensusAnalyserException(CensusAnalyserException.exceptionType.WRONG_DELIMETER_HEADER_IN_FILE,"Wrong Delimeter Or Header In File");
         }
         return noOfEntries;
     }
