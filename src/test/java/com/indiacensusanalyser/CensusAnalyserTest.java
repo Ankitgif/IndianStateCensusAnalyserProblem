2284fc7a6f0ecdaef2bccd7b58a4bea91455be4e
Ankitgif <ankitsinha2303@gmail.com>
2020-05-05 21:02:18 +0530

    [Ankit] Added UC1->TC-1.2, given wrong file throw should custom exception

--- src/test/java/com/indiacensusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/indiacensusanalyser/CensusAnalyserTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class CensusAnalyserTest {
     public final String INDIA_CENSUS_CSV_FILE_PATH = "E:\\StateCensusData.csv";
+    public final String WRONG_CSV_FILE_PATH = "E:\\StateCensusDa.csv";
 
     //UC1-->TC-1.1
     @Test
@@ -16,6 +17,16 @@ public class CensusAnalyserTest {
             Assert.assertEquals(29,numOfRecord);
         } catch (CensusAnalyserException exception) {
             exception.printStackTrace();
+        }
+    }
+    //UC-1-->TC-1.2
+    @Test
+    public void givenIndianCensusData_WithWrongFile_ShouldThrow_CustomException(){
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        try {
+            censusAnalyser.loadCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException exception) {
+            Assert.assertEquals("File Not Found",exception.getMessage());
         }
     }
 }
