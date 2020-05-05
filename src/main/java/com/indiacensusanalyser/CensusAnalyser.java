3f0b3166bca29581c6ded532605d7ac9ee3650c0
Ankitgif <ankitsinha2303@gmail.com>
2020-05-06 00:07:10 +0530

    [Ankit] Refactored Code to handle DRY violation at the Iterator Count

--- src/main/java/com/indiacensusanalyser/CensusAnalyser.java
+++ src/main/java/com/indiacensusanalyser/CensusAnalyser.java
@@ -15,9 +15,7 @@ public class CensusAnalyser {
     public int loadCensusData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             Iterator<CensusAnalyserBean> censusAnalyserBeanIterator = this.getCSVFileIterator(reader, CensusAnalyserBean.class);
-            Iterable<CensusAnalyserBean> censusAnalyserBeanIterable = () -> censusAnalyserBeanIterator;
-            int numOfEnteries = (int) StreamSupport.stream(censusAnalyserBeanIterable.spliterator(), false).count();
-            return numOfEnteries;
+            return this.getCount(censusAnalyserBeanIterator);
         } catch (IOException exception) {
             throw new CensusAnalyserException(CensusAnalyserException.exceptionType.CENSUS_FILE_PROBLEM, "File Not Found");
         } catch (RuntimeException exception) {
@@ -28,14 +26,18 @@ public class CensusAnalyser {
     public int loadStateCodeData(String csvFilePath) throws CensusAnalyserException{
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
             Iterator<CSVStatesBean> csvStatesBeanIterator = this.getCSVFileIterator(reader,CSVStatesBean.class);
-            Iterable<CSVStatesBean> csvStatesBeanIterable = () -> csvStatesBeanIterator;
-            int numOfEnteries = (int) StreamSupport.stream(csvStatesBeanIterable.spliterator(),false).count();
-            return numOfEnteries;
+            return this.getCount(csvStatesBeanIterator);
         }catch (IOException exception){
             throw new CensusAnalyserException(CensusAnalyserException.exceptionType.CENSUS_FILE_PROBLEM,"File Not Found");
         } catch (RuntimeException exception){
             throw new CensusAnalyserException(CensusAnalyserException.exceptionType.WRONG_DELIMETER_HEADER_IN_FILE,"Wrong Delimeter Or Header In File");
         }
+    }
+
+    private <E> int getCount(Iterator<E> iterator){
+        Iterable<E> csvIterable = () -> iterator;
+        int numOfEnteries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+        return numOfEnteries;
     }
 
     private <E> Iterator<E> getCSVFileIterator(Reader reader,
