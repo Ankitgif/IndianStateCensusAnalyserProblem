eecd660e3d4e66052f6c1836b4a687d917c21234
Ankitgif <ankitsinha2303@gmail.com>
2020-05-05 21:18:23 +0530

    [Ankit] Added UC1->TC-1.4, given proper csv file with improper delimeter should throw custom exception

--- src/test/java/com/indiacensusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/indiacensusanalyser/CensusAnalyserTest.java
@@ -7,6 +7,7 @@ public class CensusAnalyserTest {
     public final String INDIA_CENSUS_CSV_FILE_PATH = "E:\\StateCensusData.csv";
     public final String WRONG_CSV_FILE_PATH = "E:\\StateCensusDa.csv";
     public final String WRONG_CSV_FILE_TYPE_PATH = "E:\\StateCensusData.txt";
+    public final String WRONG_CSV_FILE_DELIMETER_PATH = "C:\\Users\\ANKIT\\Desktop\\IndianStateCensusAnalyser\\StateCensusData.csv";
 
     //UC1-->TC-1.1
     @Test
@@ -38,6 +39,16 @@ public class CensusAnalyserTest {
             censusAnalyser.loadCensusData(WRONG_CSV_FILE_TYPE_PATH);
         } catch (CensusAnalyserException exception) {
             Assert.assertEquals("File Not Found",exception.getMessage());
+        }
+    }
+    //UC-1-->TC-1.4
+    @Test
+    public void givenIndianCensusData_Proper_WithImproperDelimeter_ShouldThrow_CustomException(){
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        try {
+            censusAnalyser.loadCensusData(WRONG_CSV_FILE_DELIMETER_PATH);
+        } catch (CensusAnalyserException exception) {
+            Assert.assertEquals("Wrong Delimeter Or Header In File",exception.getMessage());
         }
     }
 }
