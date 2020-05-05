988e0598f19fb3114c86a79aacdd45c967e8d008
Ankitgif <ankitsinha2303@gmail.com>
2020-05-05 20:43:27 +0530

    [Ankit] Added to load and analyse csv file

---
+++ src/main/java/com/indiacensusanalyser/CensusAnalyser.java
@@ -0,0 +1,34 @@
+package com.indiacensusanalyser;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class CensusAnalyser {
+
+    private int noOfEntries;
+
+    public int loadCensusData(String path) throws CensusAnalyserException {
+
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(path));
+            CsvToBean<CensusAnalyserBean> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CensusAnalyserBean.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CensusAnalyserBean> csvUserIterator = csvToBean.iterator();
+            while (csvUserIterator.hasNext()) {
+                noOfEntries++;
+                CensusAnalyserBean csvUser = csvUserIterator.next();
+            }
+        } catch (IOException exception) {
+            throw new CensusAnalyserException(CensusAnalyserException.exceptionType.CENSUS_FILE_PROBLEM,"File Not Found");
+        }
+        return noOfEntries;
+    }
+}
