6f5af7ed8cb51ea4bb687bd4151ab4f5dbeee8d2
Ankitgif <ankitsinha2303@gmail.com>
2020-05-07 10:12:30 +0530

    [Ankit] Added to throws CSVBuilderException

---
+++ src/main/java/com/csvbuilderhandler/ICSVBuilder.java
@@ -0,0 +1,10 @@
+package com.csvbuilderhandler;
+
+import com.csvbuilderhandler.CSVBuilderException;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public interface ICSVBuilder<E> {
+    public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException;
+}
