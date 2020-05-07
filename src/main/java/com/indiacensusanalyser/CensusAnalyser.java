cde1e6a22b8500d7913295c469393931a8ee1d11
Ankitgif <ankitsinha2303@gmail.com>
2020-05-07 14:21:04 +0530

    [Ankit] Added code to sort the Census Data by State

--- src/main/java/com/indiacensusanalyser/CensusAnalyser.java
+++ src/main/java/com/indiacensusanalyser/CensusAnalyser.java
@@ -3,11 +3,13 @@ package com.indiacensusanalyser;
 import com.csvbuilderhandler.CSVBuilderException;
 import com.csvbuilderhandler.CSVBuilderFactory;
 import com.csvbuilderhandler.ICSVBuilder;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.StreamSupport;
@@ -39,6 +41,34 @@ public class CensusAnalyser {
             throw new CensusAnalyserException(CensusAnalyserException.exceptionType.WRONG_DELIMETER_HEADER_IN_FILE,"Wrong Delimeter Or Header In File");
         } catch (CSVBuilderException exception) {
             throw new CensusAnalyserException(exception.getMessage(),exception.type.name());
+        }
+    }
+
+    public String getStateWiseSortedCensusData(String csvFilePath) throws CensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            List<CensusAnalyserBean> csvStatesBeanList = csvBuilder.getCSVFileList(reader, CensusAnalyserBean.class);
+            Comparator<CensusAnalyserBean> censusComparator = Comparator.comparing(census -> census.state);
+            this.sort(csvStatesBeanList, censusComparator);
+            String sortedStateCensusJson = new Gson().toJson(csvStatesBeanList);
+            return sortedStateCensusJson;
+        } catch (IOException exception){
+            throw new CensusAnalyserException(CensusAnalyserException.exceptionType.CENSUS_FILE_PROBLEM,"File Not Found");
+        } catch (CSVBuilderException exception) {
+            throw new CensusAnalyserException(exception.getMessage(),exception.type.name());
+        }
+    }
+
+    private void sort(List<CensusAnalyserBean> censusList, Comparator<CensusAnalyserBean> censusComparator){
+        for(int i=0;i<censusList.size()-i;i++){
+            for (int j=0;j<censusList.size()-i-1;j++){
+                CensusAnalyserBean census1 = censusList.get(j);
+                CensusAnalyserBean census2 = censusList.get(j+1);
+                if(censusComparator.compare(census1,census2) > 0){
+                    censusList.set(j, census2);
+                    censusList.set(j+1, census1);
+                }
+            }
         }
     }
 }
