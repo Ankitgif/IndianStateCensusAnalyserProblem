2acf806c237410118bb689b96267f12738c48303
Ankitgif <ankitsinha2303@gmail.com>
2020-05-07 10:22:57 +0530

    [Ankit] Added Constructor

--- src/main/java/com/indiacensusanalyser/CensusAnalyserException.java
+++ src/main/java/com/indiacensusanalyser/CensusAnalyserException.java
@@ -1,11 +1,19 @@
 package com.indiacensusanalyser;
 
+import jdk.nashorn.internal.objects.NativeBoolean;
+
 public class CensusAnalyserException extends Exception {
     private final exceptionType type;
 
     public enum exceptionType{
-        CENSUS_FILE_PROBLEM,WRONG_DELIMETER_HEADER_IN_FILE,UNABLE_TO_PARSE
+        CENSUS_FILE_PROBLEM,WRONG_DELIMETER_HEADER_IN_FILE,UNABLE_TO_PARSE;
+
     };
+
+    public CensusAnalyserException(String message, String name){
+        super(message);
+        this.type = exceptionType.valueOf(name);
+    }
     CensusAnalyserException(exceptionType type,String message){
         super(message);
         this.type = type;
