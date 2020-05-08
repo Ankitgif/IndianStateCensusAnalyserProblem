0e78373cb8add1530a6876e46bfcc028bd86a0b9
Ankitgif <ankitsinha2303@gmail.com>
2020-05-08 18:22:30 +0530

    [Ankit] Added method declaration getCSVFileIterator

--- src/main/java/com/csvbuilderhandler/ICSVBuilder.java
+++ src/main/java/com/csvbuilderhandler/ICSVBuilder.java
@@ -3,8 +3,13 @@ package com.csvbuilderhandler;
 import java.io.Reader;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public interface ICSVBuilder<E> {
 
+    public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException;
     public List<E> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException;
+
+   // public Map<String , E> getCSVFileMap(Reader reader, Class csvClass) throws CSVBuilderException;
+
 }
