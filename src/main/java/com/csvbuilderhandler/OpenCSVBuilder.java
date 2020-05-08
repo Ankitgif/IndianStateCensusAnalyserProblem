5798e5fe735221354c02e4e27578aa7724156618
Ankitgif <ankitsinha2303@gmail.com>
2020-05-08 18:24:16 +0530

    [Ankit] Added implementation of getCSVFileIterator method

--- src/main/java/com/csvbuilderhandler/OpenCSVBuilder.java
+++ src/main/java/com/csvbuilderhandler/OpenCSVBuilder.java
@@ -6,14 +6,25 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.Reader;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class OpenCSVBuilder<E> implements ICSVBuilder {
+
+    @Override
+    public Iterator getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException {
+        return this.getCSVBean(reader, csvClass).iterator();
+    }
 
     @Override
     public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
         return this.getCSVBean(reader, csvClass).parse();
 
     }
+
+//    @Override
+//    public Map getCSVFileMap(Reader reader, Class csvClass) throws CSVBuilderException {
+//        return (Map) this.getCSVBean(reader, csvClass).parse();
+//    }
 
     private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuilderException {
         try{
